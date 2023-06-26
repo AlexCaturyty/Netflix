c5055c2b33e3603537321629cd63aa8898dd5d95
AlexCaturyty <olucascavalcanti@gmail.com>
2023-06-26 13:45:53 -0300

    Ajustes na home

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    if (window.scrollY > 0) {
+        header.style.background = "linear-gradient(to bottom, black, #111111)";
+    } else {
+        header.style.background = "linear-gradient(to bottom, rgba(0, 0, 0, 0.9), transparent)";
+    }
+});
